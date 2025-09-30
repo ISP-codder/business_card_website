b98962b9f0a35f2c041c057e83251f6a44834f46
Daniil <daniil.tkachenko.05@mail.ru>
2025-09-30 21:35:27 +0300

    Updates

---
+++ assets/Abouts-BIL0-9uA.js
@@ -0,0 +1,1 @@
+import{a as e,i as t,o as n,t as r}from"./index-DO7wOGew.js";var i=n(t());e();var a=n(r());function o(){let e=(0,i.c)(1),t;return e[0]===Symbol.for(`react.memo_cache_sentinel`)?(t=(0,a.jsx)(`div`,{className:`font-display`,children:`Abouts`}),e[0]=t):t=e[0],t}export{o as default};
